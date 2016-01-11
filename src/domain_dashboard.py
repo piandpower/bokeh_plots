87b424932e491a2da62fb0044185df55ccc20832
Brittain Hard <brittainhard@gmail.com>
2016-01-11 13:26:44 -0600

    All the components for domain dashboard are present.

--- src/domain_dashboard.py
+++ src/domain_dashboard.py
@@ -5,7 +5,7 @@ from collections import Counter
 from bokeh.plotting import figure, show, output_file
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Panel, Tabs, Button
+from bokeh.models.widgets import Panel, Tabs, Button, DataTable, DateFormatter, TableColumn
 from bokeh.charts import Bar
 from bokeh.io import vform
 
@@ -14,26 +14,6 @@ from data import data as sample
 
 PLOT_ELEMENTS = 10
 BAR_WIDTH = 0.4
-
-
-def sample_tabs():
-    from bokeh.models.widgets import Panel, Tabs
-    from bokeh.io import output_file, show
-    from bokeh.plotting import figure
-
-    output_file("slider.html")
-
-    p1 = figure(plot_width=300, plot_height=300)
-    p1.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
-    tab1 = Panel(child=p1, title="circle")
-
-    p2 = figure(plot_width=300, plot_height=300)
-    p2.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
-    tab2 = Panel(child=p2, title="line")
-
-    tabs = Tabs(tabs=[ tab1, tab2 ])
-
-    return tabs
 
 
 def domains_bar(response):
@@ -54,7 +34,39 @@ def domains_bar(response):
     return bar_panel
 
 
+def domains_table(response):
+    urls = [x[0][0] for x in response["pages"]]
+
+    parsed_urls = [urlparse(x).hostname for x in urls]
+    domains = Counter(parsed_urls).most_common()
+
+    xdomains = [x[0] for x in domains]
+    ydomains = [y[1] for y in domains]
+    source = ColumnDataSource(data=dict(x=xdomains, y=ydomains))
+
+    columns = [
+            TableColumn(field="y", title="Domain"),
+            TableColumn(field="x", title="Count"),
+        ]
+    data_table = DataTable(source=source, columns=columns, width=400,
+            height=280)
+    return data_table
+
+
 def top_level_domains_bar(response):
+    urls = [x[0][0] for x in response["pages"]]
+    parsed_urls = [urlparse(x).hostname for x in urls]
+    endings = Counter([x[x.rfind("."):] for x in parsed_urls]).most_common(PLOT_ELEMENTS)
+    xendings = [x[0] for x in endings]
+    yendings = [y[1] for y in endings]
+    source = ColumnDataSource(data=dict(x=xendings, y=yendings))
+    p = Bar(source.data, values="y", label="x",
+            title="Top Level Domains by Number", bar_width=BAR_WIDTH)
+    bar_panel = Panel(child=p, title="Top Level")
+    return bar_panel
+
+
+def top_level_domains_table(response):
     urls = [x[0][0] for x in response["pages"]]
 
     parsed_urls = [urlparse(x).hostname for x in urls]
@@ -62,18 +74,20 @@ def top_level_domains_bar(response):
 
     xendings = [x[0] for x in endings]
     yendings = [y[1] for y in endings]
-
     source = ColumnDataSource(data=dict(x=xendings, y=yendings))
+    columns = [
+            TableColumn(field="y", title="Top Level Domain"),
+            TableColumn(field="x", title="Count"),
+        ]
+    data_table = DataTable(source=source, columns=columns, width=400,
+            height=280)
+    return data_table
 
-    p = Bar(source.data, values="y", label="x",
-            title="Top Level Domains by Number", bar_width=BAR_WIDTH)
-    bar_panel = Panel(child=p, title="Top Level")
-    return bar_panel
 
-
-def dashboard():
-    button = Button(label="Foo", type="success")
+def domain_dashboard():
     domains = domains_bar(sample)
     top_level = top_level_domains_bar(sample)
-    return components(vform(Tabs(tabs=[domains, top_level]), button))
-    #return components(sample())
+    table_domains = domains_table(sample)
+    top_level_table_domains = top_level_domains_table(sample)
+    return components(vform(Tabs(tabs=[domains, top_level]), table_domains,
+        top_level_table_domains))
--- src/server.py
+++ src/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from domain_dashboard import dashboard
+from domain_dashboard import domain_dashboard
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    script, div = dashboard()
+    script, div = domain_dashboard()
     return render_template("index.html", div=div, script=script)
 
 if __name__ == '__main__':
