e0e276d5540e568f06445ee087fadb4301086645
Brittain Hard <brittainhard@gmail.com>
2016-01-13 09:33:57 -0600

    Organize plot elements with vbox, hbox.

--- src/server.py
+++ src/server.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
-from domain_dashboard import domain_dashboard
+from flask import Flask, render_template, url_for
+
+from domains_dashboard import domains_dashboard
 from keywords_dashboard import keywords_dashboard
+
+from data import page_data
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    script, div = keywords_dashboard()
+    script, div = domains_dashboard(page_data)
     return render_template("index.html", div=div, script=script)
 
 
